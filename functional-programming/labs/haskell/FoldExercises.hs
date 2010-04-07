034f1100e253d3f1210a1a4aee162be897a20e3c
Bas Bossink <bas.bossink@gmail.com>
2010-04-07 02:23:51 +0200

    Add FoldExercises skeleton.

---
+++ functional-programming/labs/haskell/FoldExercises.hs
@@ -0,0 +1,11 @@
+module FoldExercises
+where
+{-- 
+write alternatives to the standard concat, takeWhile and groupBy functions
+using a fold 
+--} 
+fconcat = undefined
+
+ftakeWhile = undefined
+
+fgroupBy = undefined
