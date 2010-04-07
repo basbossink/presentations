af5852efb2ed33446aefe70a4125fb69b2ece0c2
Bas Bossink <bas.bossink@gmail.com>
2010-04-07 02:16:08 +0200

    Add FoldExercises the solutions.

--- functional-programming/labs/haskell/FoldExercises.hs
+++ functional-programming/labs/haskell/FoldExercises.hs
@@ -4,8 +4,14 @@ where
 write alternatives to the standard concat, takeWhile and groupBy functions
 using a fold 
 --} 
-fconcat = undefined
 
-ftakeWhile = undefined
+fconcat = foldr (++) []
 
-fgroupBy = undefined
+ftakeWhile pred xs = foldr step [] xs 
+    where step x ys | pred x = x : ys
+                    | otherwise = []
+
+fgroupBy eq xs = foldl step [] xs
+    where step [] x = [[x]]
+          step ys x = if eq ((head . last) ys) x  then (init ys) ++ [(last ys) ++ [x]]
+                      else ys ++ [[x]]
