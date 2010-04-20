7121aaa9a11f17f733e2f36ccd7b3b85057725da
Bas Bossink <bas.bossink@gmail.com>
2010-04-20 02:23:00 +0200

    First rev of fsharp labs.

---
+++ functional-programming/labs/fsharp/Implementation/FoldExcercises.fs
@@ -0,0 +1,13 @@
+﻿#light
+namespace FoldExcercises
+
+module FoldExcercises =
+
+/// write alternatives to the standard concat, takeWhile and groupBy functions
+/// using a fold 
+
+
+    let concat xs = []
+    let takeWhile predicate xs = ([]: seq<int>)
+    let groupBy predicate xs = ([]: seq<int>)
+    
---
+++ functional-programming/labs/fsharp/TestsAndChecks/FoldExcerciseVerification.fs
@@ -0,0 +1,19 @@
+﻿#light
+
+open FsCheck
+open System
+open FoldExcercises
+open System.Linq
+
+let prop_concat (xs:list<list<int>>) = List.concat xs = FoldExcercises.concat (xs: list<list<int>>)
+
+let predicate x = x < 37
+let prop_takeWhile (xs: seq<int>) = xs.TakeWhile predicate = FoldExcercises.takeWhile predicate xs
+
+Console.WriteLine("----------Check all toplevel properties----------------");
+type Marker = member x.Null = ()
+//if there are instances defined: (throws exception if not)
+//overwriteGeneratorsByType (typeof<Marker>.DeclaringType)
+quickCheckAll (typeof<Marker>.DeclaringType)
+
+Console.ReadKey() |> ignore
