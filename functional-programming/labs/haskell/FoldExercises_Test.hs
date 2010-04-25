4dc01dca7e129d5346980f915ecf1c2178d2a1dd
Bas Bossink <bas.bossink@gmail.com>
2010-04-25 22:37:00 +0200

    Added a few predicates for takeWhile property.

--- functional-programming/labs/haskell/FoldExercises_Test.hs
+++ functional-programming/labs/haskell/FoldExercises_Test.hs
@@ -9,16 +9,19 @@ import qualified FoldExercises as F
 prop_concat xs = concat xs == F.concat xs
     where types = xs ::[[Int]]
 
-predicate x = x < 37
-prop_takeWhile xs = takeWhile predicate xs == F.takeWhile predicate xs
-    where types = xs::[Int] 
+prop_takeWhile predicate xs = 
+    takeWhile predicate xs == F.takeWhile predicate xs
+    where types = xs::[Int]
 
 prop_groupBy predicate xs = groupBy predicate xs == F.groupBy predicate xs
     where types = xs::[Int]
 
 equal x y = (y `mod` 3) == 0 
+
 intListChecks = [ 
-              prop_takeWhile,
+              prop_takeWhile (\x -> x < 37),
+              prop_takeWhile (\x -> x > 37),
+              prop_takeWhile (\x -> x /= 37),
               prop_groupBy (==),
               prop_groupBy (/=),
               prop_groupBy equal
